500df569fcbca6b0fd433fab759a60f14976b6d4
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-08 17:14:21 +0200

    dadan navbar sa rutiranjem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent } from './home/home.component'
+import {LoginComponent } from './login/login.component'
+import {ContactComponent } from './contact/contact.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component:  HomeComponent },
+    { path: 'login', component:  LoginComponent },
+    { path: 'contact', component:  ContactComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
