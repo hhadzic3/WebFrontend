c1c9e5a099cf673b3d5593851676776e1c613398
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-18 18:34:04 +0200

    popravka kontejnera za ispis kolekcija

--- src/app/manager/vehicles/vehicles.component.ts
+++ src/app/manager/vehicles/vehicles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
 
 export interface PeriodicElement {
   name: string;
@@ -27,14 +28,38 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class VehiclesComponent implements OnInit {
 
-  constructor() { }
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-
+  displayedColumns: string[] = ['select','position', 'name', 'weight', 'symbol' ];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
   }
