0bf3f7e3f16d9e3112193661deee19f00c6c4ba4
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-26 09:35:13 +0200

    Dizajn ispisa zavrsenih pregleda

--- src/app/mechanic/rewievs/rewievs.component.ts
+++ src/app/mechanic/rewievs/rewievs.component.ts
@@ -2,23 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
+  vehicle: string;
   position: number;
-  weight: number;
-  symbol: string;
+  type: string;
+  owner: string;
+  rewiev_done_by: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, vehicle: 'Hydrogen', type: "transport", owner: 'H' ,rewiev_done_by: 'Hamo' },
+  {position: 2, vehicle: 'Helium', type: "transport", owner: 'He' , rewiev_done_by: 'Hamo'}
 ];
 @Component({
   selector: 'app-rewievs',
@@ -26,7 +19,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./rewievs.component.css']
 })
 export class RewievsComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'vehicle', 'type', 'owner' , 'rewiev_done_by'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(event: Event) {
