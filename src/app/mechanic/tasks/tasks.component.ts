29cdac447626821266f0ae04cf52ca98ce8d371a
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-26 09:23:17 +0200

    Dizajn taskova

--- src/app/mechanic/tasks/tasks.component.ts
+++ src/app/mechanic/tasks/tasks.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class TasksComponent implements OnInit {
   panelOpenState = false;
   constructor() { }
-  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  typesOfThings: string[] = [ "Idetifikacija osobe i vozila (provjera dokumentacije i identiteta vlasnika i vozila)"
+    ,   "Vizuelna inspekcija automobila","Detaljna analiza"
+    ,   "Elektrika",   "Motor i izduvni sistem",   "Upravljački mehanizam / vješanje"
+    ,   "Sistem za prijenos snage / transmisija"
+    ,   "Karoserija", "Grijanje i Ventilacija",   "Gorivo"
+    ,   "Sistem punjenja",   "Sistem paljenja",   "Sistem za hlađenje"
+    ,   "Brave/alarmi",   "Kočnice",   "Stakla",   "Svjetla"];
   ngOnInit(): void {
   }
 
