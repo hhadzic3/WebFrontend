5caf458aed47bf2dc578ea01335d514cbd6f6ca0
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-26 14:00:49 +0200

    Dodana forma sa validacijom za novo vozilo

--- src/app/manager/new-vehicle/new-vehicle.component.ts
+++ src/app/manager/new-vehicle/new-vehicle.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 
 import { Observable }    from 'rxjs/Observable';
 
+
 interface Animal {
   name: string;
   sound: string;
@@ -13,14 +14,14 @@ interface Animal {
   styleUrls: ['./new-vehicle.component.css']
 })
 export class NewVehicleComponent implements OnInit {
+  startDate = new Date(1990, 0, 1);
 
   animalControl = new FormControl('', Validators.required);
   selectFormControl = new FormControl('', Validators.required);
   animals: Animal[] = [
-    {name: 'Dog', sound: 'Woof!'},
-    {name: 'Cat', sound: 'Meow!'},
-    {name: 'Cow', sound: 'Moo!'},
-    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!'},
+    {name: 'Putnicko', sound: 'Puntnicko!'},
+    {name: 'Teretno', sound: 'Teretno!'},
+    {name: 'Priključno', sound: 'Prikljucno!'}
   ];
 
   formGroup: FormGroup;
@@ -31,65 +32,34 @@ export class NewVehicleComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
-    this.setChangeValidate()
   }
 
   createForm() {
-    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    this.formGroup = this.formBuilder.group({
-      'email': [null, [Validators.required, Validators.pattern(emailregex)], this.checkInUseEmail],
-      'name': [null, Validators.required],
-      'password': [null, [Validators.required, this.checkPassword]],
-      'description': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+     this.formGroup = this.formBuilder.group({
+      'Vname': [null, Validators.required],
+      'date1': [null, Validators.required],
+      'date2': [null, Validators.required],
+      'number': [null, Validators.required],
+      'owner': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
       'validate': ''
     });
   }
 
-  setChangeValidate() {
-    this.formGroup.get('validate').valueChanges.subscribe(
-      (validate) => {
-        if (validate == '1') {
-          this.formGroup.get('name').setValidators([Validators.required, Validators.minLength(3)]);
-          this.titleAlert = "You need to specify at least 3 characters";
-        } else {
-          this.formGroup.get('name').setValidators(Validators.required);
-        }
-        this.formGroup.get('name').updateValueAndValidity();
-      }
-    )
-  }
 
-  get name() {
-    return this.formGroup.get('name') as FormControl
+  get Vname() {
+    return this.formGroup.get('Vname') as FormControl
   }
-
-  checkPassword(control) {
-    let enteredPassword = control.value
-    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
+  get date1() {
+    return this.formGroup.get('date1') as FormControl
   }
-
-  checkInUseEmail(control) {
-    // mimic http database access
-    let db = ['tony@gmail.com'];
-    return new Observable(observer => {
-      setTimeout(() => {
-        let result = (db.indexOf(control.value) !== -1) ? { 'alreadyInUse': true } : null;
-        observer.next(result);
-        observer.complete();
-      }, 4000)
-    })
+  get date2() {
+    return this.formGroup.get('date2') as FormControl
   }
-
-  getErrorEmail() {
-    return this.formGroup.get('email').hasError('required') ? 'Field is required' :
-      this.formGroup.get('email').hasError('pattern') ? 'Not a valid emailaddress' :
-        this.formGroup.get('email').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
+  get number() {
+    return this.formGroup.get('number') as FormControl
   }
-
-  getErrorPassword() {
-    return this.formGroup.get('password').hasError('required') ? 'Field is required (at least eight characters, one uppercase letter and one number)' :
-      this.formGroup.get('password').hasError('requirements') ? 'Password needs to be at least eight characters, one uppercase letter and one number' : '';
+  get owner() {
+    return this.formGroup.get('owner') as FormControl
   }
 
   onSubmit(post) {
