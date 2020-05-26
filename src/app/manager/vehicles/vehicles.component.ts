6604e24f02170ef71ddf9c460927199469d5d51c
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-26 09:43:23 +0200

    Dizajn za menadzera

--- src/app/manager/vehicles/vehicles.component.ts
+++ src/app/manager/vehicles/vehicles.component.ts
@@ -2,24 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {SelectionModel} from '@angular/cdk/collections';
 
+
 export interface PeriodicElement {
-  name: string;
+  vehicle: string;
   position: number;
-  weight: number;
-  symbol: string;
+  type: string;
+  owner: string;
+  rewiev_done_by: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, vehicle: 'Hydrogen', type: "transport", owner: 'H' ,rewiev_done_by: 'Hamo' },
+  {position: 2, vehicle: 'Helium', type: "transport", owner: 'He' , rewiev_done_by: 'Hamo'}
 ];
 @Component({
   selector: 'app-vehicles',
@@ -28,7 +22,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class VehiclesComponent implements OnInit {
 
-  displayedColumns: string[] = ['select','position', 'name', 'weight', 'symbol' ];
+  displayedColumns: string[] = ['select','position',  'vehicle', 'type', 'owner' , 'rewiev_done_by'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
